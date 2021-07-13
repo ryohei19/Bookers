e67aebcc978ff6e4e46361092d0b2dc93cb6dd69
ryohei19 <ryohei.basket@gmail.com>
2021-07-13 16:52:22 +0000

    make destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,12 @@ class BooksController < ApplicationController
     redirect_to book_path
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
+  end
+
 
 
   private

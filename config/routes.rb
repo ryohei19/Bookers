a6a6b86db054f44d2fab706ac3d95b83ee0ce7c2
ryohei19 <ryohei.basket@gmail.com>
2021-07-13 03:05:59 +0000

    make index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'homes/top'
+  get 'books' => 'books#index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
